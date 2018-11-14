212094fa1731af51b12c7470ac43b73df28419b6
victoiredmdt <45006106+victoiredmdt@users.noreply.github.com>
2018-11-14 15:27:12 +0100

    Add files via upload

--- twitter_connection_setup.py
+++ twitter_connection_setup.py
@@ -25,7 +25,6 @@ def twitter_setup():
     api = tweepy.API(auth)
     return api
 
-print(twitter_setup())
 
 def collect():
     connexion = twitter_setup()
@@ -33,6 +32,38 @@ def collect():
     for tweet in tweets:
         print(tweet.text)
 
+def collect_by_user(user_id):
+    connexion = twitter_setup()
+    statuses = connexion.user_timeline(id = user_id, count = 200)
+    for status in statuses:
+        print(status.text)
+    return statuses
+
+
+'''print(collect_by_user(151304840))'''
+
+from tweepy.streaming import StreamListener
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        if  str(status) == "420":
+            print(status)
+            print("You exceed a limited number of attempts to connect to the streaming API")
+            return False
+        else:
+            return True
+
+def collect_by_streaming():
+    connexion = twitter_setup()
+    listener = StdOutListener()
+    stream=tweepy.Stream(auth = connexion.auth, listener=listener)
+    stream.filter(track=['Emmanuel Macron'])
+
+print(collect_by_streaming())
 
 
 
